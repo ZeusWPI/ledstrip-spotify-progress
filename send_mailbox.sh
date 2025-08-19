b8b6054d9dbe881af6d8d0bdcca339b6ce3d7f94
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2025-08-19 16:35:04 +0200

    edit send_mailbox.sh to use xh instead of curl

--- send_mailbox.sh
+++ send_mailbox.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -X PUT -H "Content-Type: application/json" --data "{\"message\": \"$1\", \"topic\":  \"spotify_progress\"}" http://ledstrip/api/mailbox.json
+xh put http://ledstrip/api/mailbox.json topic=spotify_progress message="$1"
 
